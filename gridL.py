5b5736476d0a1b42a39c769d657c88361ff6ff12
sam wallace <samwallace949@gmail.com>
2019-06-29 21:56:29 -0400

    Completion of model backlog text

--- gridL.py
+++ gridL.py
@@ -6,6 +6,8 @@ from gridL_agents import agent
 from gridL_buffer import experience_buffer
 from gridL_buffer import buffer_image
 from gridL_game import gameData
+
+model_desc_file = "saved_run_backlog.txt"
 def time_string():
 	t = time.gmtime()
 	s = ""
@@ -23,15 +25,16 @@ b = experience_buffer(1000000)
 random_player = agent(experience_buffer(1000000), game, 1)
 trainables = tf.trainable_variables()
 trained_player = agent(b, game, 1)
-for i in range (0, 20000):
-	trained_player.make_action(trained_player.get_logits(), 1-(max(i-10000, 0)/10000))
+for i in range (0, 40000):
+	trained_player.make_action(trained_player.get_logits(), 1-(max(i-20000, 0)/20000))
 	if((i+1)%100 == 0):
 		print("{} rounds completed".format(i))
-	if ((i+1) % 1000 == 0 and i > 10000):
-		trained_player.train(0.1, 100, 10)
+	if ((i+1) % 1000 == 0 and i > 20000):
+		trained_player.train(0.1, 200, 10)
 game.reset_game()
 rand_wins, trained_wins = 0,0
-for j in range (0, 10):
+num_games = 300
+for j in range (0, num_games):
 	game.reset_game()
 	turn,c = 0,0
 	while game.is_over() == 0:
@@ -57,12 +60,18 @@ for j in range (0, 10):
 		else:
 			trained_wins += 1
 			print ("Trained Win!")
-print("After 100 games, the agent trained for 100000 steps has a win ratio of {} to {} against a randomly playing agent".format(trained_wins, rand_wins))
-description = "random samples cached for ten thousand steps, then agent trains with a linear progression in its certainty of deciding the move throughout another ten thousand frames."
+print("After {} games, the agent trained for 4,000 steps has a win ratio of {} to {} against a randomly playing agent".format(num_games, trained_wins, rand_wins))
+description = "random samples cached for twenty thousand steps, then agent trains with a linear progression in its certainty of deciding the move throughout another twenty thousand frames, 200 frames every 1000 cached (4,000 total)"
 print(description)
 print(time_string())
 save_desc = input("do you want to save the results of this test? (y/n) ")
 if(save_desc == "y"):
 	run_name = input("Name of Run: ")
 	opp_name = input("Name of Opponent: ")
-	s = "Name of Run: {}\nName of Opponent: {}\nDescription: {}\nDate and Time of Run: {}\nWins: {}\nLosses: {}\n".format(run_name, opp_name, description, time_string(), trained_wins, rand_wins)
+	s = "\n\n\n\nName of Run: {}\nName of Opponent: {}\nDescription: {}\nDate and Time of Run: {}\nWins: {}\nLosses: {}\n".format(run_name, opp_name, description, time_string(), trained_wins, rand_wins)
+	f = open(model_desc_file, "r")
+	existing_data = f.read()
+	f.close()
+	f = open(model_desc_file, "w")
+	f.write(existing_data+s)
+	f.close()
