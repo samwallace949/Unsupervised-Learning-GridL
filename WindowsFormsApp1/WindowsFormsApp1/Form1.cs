5eb8b66eb3109d45dad62c14074912ce62e5c9da
sam wallace <samwallace949@gmail.com>
2019-08-02 18:49:08 -0400

    created button grid

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,7 +14,25 @@ namespace WindowsFormsApp1
     {
         public Form1()
         {
+            int buttonSize = 20, padding = 2, gridOffsetX = 100, gridOffestY = 30;
             InitializeComponent();
+            for(int i = 0; i < 7; i++) {
+                for (int j = 0; j < 7; j++) {
+                    Button b = new Button();
+                    this.Controls.Add(b);
+                    b.SetBounds(
+                        (i * (buttonSize + padding)) + gridOffsetX, 
+                        (j * (buttonSize + padding)) + gridOffestY, 
+                        buttonSize, 
+                        buttonSize);
+                    
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
